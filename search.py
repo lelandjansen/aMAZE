2f9710f7fbab94b5478c222e6d8bb8163151023c
Leland Jansen <lelandjansen@users.noreply.github.com>
2016-04-02 15:29:11 -0600

    Working on variable-difficulty AI

--- search.py
+++ search.py
@@ -7,9 +7,8 @@
 
 import sys
 import random
-# random.seed(0) # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DELETE THIS
+# random.seed(0)
 
-# from generateMaze import Maze
 from generateMaze import Maze
 
 recursionLimit = 1000000
@@ -17,9 +16,11 @@ sys.setrecursionlimit(recursionLimit)
 
 
 # "Smart" AI
-def find_path(graph, start, end, path=[]):
+def find_path(maze, start, end, path=[]):
     # Source:
     #  https://www.python.org/doc/essays/graphhs/
+
+    graph = maze.graph()
 
     if start not in graph:
         KeyError("Start node " + str(start) + " not in maze.")
@@ -37,52 +38,17 @@ def find_path(graph, start, end, path=[]):
     return None
 
 
-# "Kinda smart" AI
-def dfs_path(graph, start, end):
+
+
+
+def dfsPath(maze, start, end, mode):
+
+    graph = maze.graph()
 
     if start not in graph:
         KeyError("Start node " + str(start) + " not in maze.")
     if end not in graph:
         KeyError("End node "   + str(end)   + " not in maze.")
-
-
-    path    = [start]
-    visited = set(start)
-    found   = False
-
-    def dfs(node, end):
-        visited.add(node)
-        nextNode = graph[node]
-        random.shuffle(nextNode)
-        for succ in nextNode:
-            if succ == end:
-                dfs.found = True
-                dfs.path += [end]
-                return
-            if succ not in visited:
-                if not dfs.found:
-                    dfs.path += [succ]
-                dfs(succ, end)
-                if not dfs.found:
-                    dfs.path += [succ]
-
-    dfs.path    = path
-    dfs.visited = visited
-    dfs.found   = found
-
-    dfs(start, end)
-
-    return path
-
-
-# "A bit sillier smart" AI
-# Objective: Choose the path that will take the AI furthest from the goal
-def dfs_path_silly(graph, start, end):
-    if start not in graph:
-        KeyError("Start node " + str(start) + " not in maze.")
-    if end not in graph:
-        KeyError("End node "   + str(end)   + " not in maze.")
-
 
     path    = [start]
     visited = set(start)
@@ -92,8 +58,12 @@ def dfs_path_silly(graph, start, end):
         visited.add(node)
         nextNode = graph[node]
 
-        nextNode.sort(key=lambda x: abs(x[0]-start[0]) + abs(x[1]-start[1]) )
-        # print(nextNode)
+        if mode is 1: # random next node
+            nextNode.sort(key=lambda x: abs(x[0]-start[0]) + abs(x[1]-start[1]))
+        elif mode is 2: # "worst" next node
+            random.shuffle(nextNode)
+        elif mode is 3: # "best" next node
+            nextNode.sort(key=lambda x: abs(x[0]-end[0]) + abs(x[1]-end[1]))
 
         for succ in nextNode:
             if succ == end:
@@ -114,60 +84,6 @@ def dfs_path_silly(graph, start, end):
     dfs(start, end)
 
     return path
-
-
-
-
-
-
-
-
-# "A bit sillier smart" AI
-# Objective: Choose the path that will take the AI furthest from the goal
-def dfs_path_smart(graph, start, end):
-    if start not in graph:
-        KeyError("Start node " + str(start) + " not in maze.")
-    if end not in graph:
-        KeyError("End node "   + str(end)   + " not in maze.")
-
-
-    path    = [start]
-    visited = set(start)
-    found   = False
-
-    def dfs(node, end):
-        visited.add(node)
-        nextNode = graph[node]
-
-        nextNode.sort(key=lambda x: abs(x[0]-end[0]) + abs(x[1]-end[1]) )
-        # print(nextNode)
-
-        for succ in nextNode:
-            if succ == end:
-                dfs.found = True
-                dfs.path += [end]
-                return
-            if succ not in visited:
-                if not dfs.found:
-                    dfs.path += [succ]
-                dfs(succ, end)
-                if not dfs.found:
-                    dfs.path += [succ]
-
-    dfs.path    = path
-    dfs.visited = visited
-    dfs.found   = found
-
-    dfs(start, end)
-
-    return path
-
-
-
-
-
-
-
 
 
 
@@ -175,7 +91,9 @@ def dfs_path_smart(graph, start, end):
 # AI has 5000 tries to reach end
 # If it is not successful, it retraces its steps to the start,
 # then uses find_path to find a way out
-def randomSearch(graph, start, end, path=[]):
+def randomSearch(maze, start, end, path=[]):
+
+    graph = maze.graph()
 
     if start not in graph:
         KeyError("Start node " + str(start) + " not in maze.")
@@ -197,52 +115,127 @@ def randomSearch(graph, start, end, path=[]):
 
 
 
-def ai(intelligence, graph, start, end):
+def ai(intelligence, maze, start, end):
     if intelligence is 1:
-        return randomSearch(graph, start, end)
+        return randomSearch(maze, start, end)
     elif intelligence is 2:
-        return dfs_path(graph, start, end)
+        return dfsPath(maze, start, end, 1)
     elif intelligence is 3:
-        return find_path(graph, start, end)
+        return dfsPath(maze, start, end, 2)
     elif intelligence is 4:
-        return aStar(graph, start, end)
+        return dfsPath(maze, start, end, 3)
     else:
-        raise ValueError("Intelligence level " + str(intelligence)
-                            + " does not exist")
+        raise ValueError("AI level " + str(intelligence) + " does not exist.")
 
 
 
 
-mazeSize = 16
-startCoord = (0,0)
-endCoord = (mazeSize-1,mazeSize-1)
+mazeSize   = (5, 10)
+startCoord = (2,3)
+endCoord   = (7,8)
 
 
-myMaze = Maze(mazeSize,mazeSize)
+myMaze = Maze(mazeSize[0],mazeSize[1])
 myMaze.generateMaze()
 myGraph = myMaze.graph()
 
 
+scoreList = []
 
-print("Smart AI:")
-print(len(find_path(myGraph, startCoord, endCoord)))
+for x in range(mazeSize[0]):
+    scoreList.append([])
+    for y in range(mazeSize[1]):
+        scoreList[x].append(abs((abs(x-endCoord[0]) + abs(y-endCoord[1]))))
 
-print()
-print("Kinda Smart Smarter AI:")
-print(len(dfs_path_smart(myGraph, startCoord, endCoord)))
+for i in range(len(scoreList)):
+    for j in range(len(scoreList[i])):
+        number = scoreList[i][j]
+        if number < 10:
+            print(" ", end="")
+        print("   " + str(number), end="")
+    print()
+    print()
 
-print()
-print("Kinda Smart AI:")
-print(len(dfs_path(myGraph, startCoord, endCoord)))
 
-print()
-print("Kinda Smart Sillier AI:")
-print(len(dfs_path_silly(myGraph, startCoord, endCoord)))
 
+# To compute farthest point (manhattan)
+# (sizeX-2(sizeX//2 - endX//2)) + (sizeY-2(sizeY//2 - endY//2))
+
+
+def biasPoint(maze, end, difficulty):
+    # maze: Maze class
+    # end: Tuple containing end point
+    # difficulty: Integer between 0 and 100
+
+    if end not in maze.graph():
+        KeyError("End point " + str(end) + " not in maze.")
+    if difficulty < 0 or 100 < difficulty:
+        ValueError("Difficulty must be between 0 and 100 (inclusive).")
+
+    size = (maze.get_sizex(), maze.get_sizey())
+
+    farthestPointScore = size[0] - 2*(size[0]//2 - end[0]//2) + \
+                         size[1] - 2*(size[1]//2 - end[1]//2)
+
+    # Compute AI bias score based on difficulty
+    biasScore = farthestPointScore - (difficulty * farthestPointScore) // 100
+
+    biasPoint = list(end)
+
+    direction = (random.choice([-1, 1]), random.choice([-1, 1]))
+
+    for d in range(biasScore):
+        i = random.choice([0, 1])
+        biasPoint[i] += direction[i]
+    biasPoint = tuple(biasPoint)
+    print(biasPoint)
+
+
+biasPoint(myMaze, endCoord, 80)
 
 
 # print()
+# myPath = dfsPath(myGraph, startCoord, endCoord, 1)
+# print("Worst path DFS:" + str(len(myPath)))
+# print(myPath)
+#
 # print()
+# myPath = dfsPath(myGraph, startCoord, endCoord, 2)
+# print("Random path DFS: " + str(len(myPath)))
+# print(myPath)
+#
+# print()
+# myPath = dfsPath(myGraph, startCoord, endCoord, 3)
+# print("Best path DFS: " + str(len(myPath)))
+# print(myPath)
+#
+# print()
+# myPath = find_path(myGraph, startCoord, endCoord)
+# print("Shortest path: " + str(len(myPath)))
+# print(myPath)
+#
+#
+#
+# print()
+# print()
+# print()
+# print()
+# print()
+# print()
+# print()
+# print()
+
+
+
+
+
+
+
+
+
+
+
+
 # print("Silly AI:")
 # print(randomSearch(myMaze, (0,0), (80,90)))
 
